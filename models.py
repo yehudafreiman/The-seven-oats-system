51681bc804cfeaa728e6ceeffdb385abe5e845a9
Yehuda Freiman <yehfre@icloud.com>
2025-11-26 17:06:57 +0200

    rename func to create_list_of_nums

--- models.py
+++ models.py
@@ -5,24 +5,21 @@ class Beds:
         self.bed = bed
         self.soldier_id = None
 
-def create_list_of_beds(num_buildings: int, num_rooms: int, num_beds: int) -> list[int]:
-    list_of_beds = []
+def create_list_of_nums(num_buildings: int, num_rooms: int, num_beds: int) -> list[int]:
+    list_of_nums = []
     for i in range(1, num_buildings+1):
         for j in range(1, num_rooms+1):
             for k in range(1, num_beds+1):
-                bed = [i, j, k]
-                list_of_beds.append(bed)
-    return list_of_beds
-print(create_list_of_beds(2,10,8))
+                l = [i, j, k]
+                list_of_nums.append(l)
+    return list_of_nums
 
 def create_bed_object():
-    all_beds = create_list_of_beds(2,10,8)
+    all_beds = create_list_of_nums(2,10,8)
     beds = []
     for b in all_beds:
         beds.append(Beds(b[0], b[1], b[2]))
     return beds
-print(create_bed_object())
-
 
 
 
